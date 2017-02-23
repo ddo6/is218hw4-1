fac4b127b6ed99f165514908c880361a0c4443a7
ddo6 <ddo6@njit.edu>
2017-02-22 19:34:46 -0500

    Update category_list.php

--- category_list.php
+++ category_list.php
@@ -31,6 +31,20 @@ $statement->closeCursor();
         </tr>
         
         <!-- add code for the rest of the table here -->
+      <aside>
+        <!-- display a list of categories -->
+        <h2>Categories</h2>
+        <nav>
+        <ul>
+            <?php foreach ($categories as $category) : ?>
+            <li><a href=".?category_id=<?php echo $category['categoryID']; ?>">
+                    <?php echo $category['categoryName']; ?>
+                </a>
+            </li>
+            <?php endforeach; ?>
+        </ul>
+        </nav>          
+    </aside>
       
     
     </table>
