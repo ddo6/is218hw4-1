8881e008c436c3e9776574be918dad80941d33fa
ddo6 <ddo6@njit.edu>
2017-02-22 20:04:43 -0500

    Update category_list.php

--- category_list.php
+++ category_list.php
@@ -32,10 +32,8 @@ $statement->closeCursor();
       <?php foreach ($categories as $category) : ?>
             <tr>
               <td><?php echo $product['productCode']; ?></td>
-              <td class="right"><?php echo $product['listPrice']; ?></td>
+              
                 <td><form action="delete_product.php" method="post">
-                    <input type="hidden" name="product_id"
-                           value="<?php echo $product['productID']; ?>">
                     <input type="hidden" name="category_id"
                            value="<?php echo $product['categoryID']; ?>">
                     <input type="submit" value="Delete">
