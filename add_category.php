99058159516d1bcba4cd6e33675908f6b8306096
ddo6 <ddo6@njit.edu>
2017-02-22 20:35:52 -0500

    Update add_category.php

--- add_category.php
+++ add_category.php
@@ -1,9 +1,7 @@
 <?php
 // Get the category data
 $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
-$code = filter_input(INPUT_POST, 'code');
-$name = filter_input(INPUT_POST, 'name');
-$price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
+
 // Validate inputs
 if ($category_id == null) {
     $error = "Invalid product data. Check all fields and try again.";
@@ -13,7 +11,7 @@ if ($category_id == null) {
     
     // Add the product to the database  
     $query = 'INSERT INTO categories
-                 (categoryID)
+                 (categoryName)
               VALUES
                  (:category_id)';
     $statement = $db->prepare($query);
